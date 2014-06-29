ab4d2abc8324243e6c9d5769bfe25dcada63b976
Robert Wallhead <rwallhead@gmail.com>
2014-06-29 01:23:05 +0000

    Working syslog logging from w1 sensors

--- logger.py
+++ logger.py
@@ -7,14 +7,44 @@ Hat-tip to https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20
 
 """
 import glob
+import itertools
+import os
+import syslog
 
 
-def log():
+SYSLOG_IDENT = 'BurPi-temperature'
+SYSLOG_FMT = '{},{}'
+
+
+def temperature(w1_slave_path):
+    """ Returns the temperature from a w1_slave file path, None otherwise.
+    """
+    try:
+        with open(w1_slave_path, 'r') as w1_f:
+            sum, temp = w1_f.read().split(os.linesep)[:2]
+        if not sum.endswith('YES'):
+            return
+        return float(temp.split('=')[1]) / 1000
+    except:
+        pass
+
+
+def cmdline():
     """ Logs all the temperature readings.
     """
     # Detect the sensors
     sensors = glob.glob('/sys/bus/w1/devices/*/w1_slave')
 
+    # Create an iterator of (serial, temperature) tuples
+    data = itertools.izip(
+        map(lambda path: path.split('/')[-2], sensors),
+        map(temperature, sensors)
+    )
+
+    # Log the temperatures to syslog
+    syslog.openlog(SYSLOG_IDENT)
+    map(lambda item: syslog.syslog(SYSLOG_FMT.format(*item)), data)
+
 
 if __name__ == '__main__':
-    log()
+    cmdline()
