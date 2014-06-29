a6e8ae4fbcd27df48aa36563ca2518d769461b42
Robert <rwallhead@gmail.com>
2014-06-29 14:15:59 +1000

    Replaced Chart.js with Flot (http://www.flotcharts.org/) as I need nullable x values

--- www/static/burpi.js
+++ www/static/burpi.js
@@ -1,37 +1,14 @@
-function update(chart) {
-    // Global Line options
-    var options = {
-        animation: false,
-        datasetFill : false,
+function update() {
+    var d1 = [];
+    for (var i = 0; i < 14; i += 0.5) {
+        d1.push([i, Math.sin(i)]);
     }
-
-    var data = {
-        labels : ["January","February","March","April","May","June","July"],
-        datasets : [
-            {
-                strokeColor : "rgba(220,220,220,1)",
-                pointColor : "rgba(220,220,220,1)",
-                pointStrokeColor : "#fff",
-                data : [65,59,90,81,56,55,40]
-            },
-            {
-                strokeColor : "rgba(151,187,205,1)",
-                pointColor : "rgba(151,187,205,1)",
-                pointStrokeColor : "#fff",
-                data : [28,48,40,19,96,27,100]
-            }
-        ]
-    }
-    chart.Line(data, options);
+    $.plot("#chart-placeholder", [ d1 ]);
 }
 
 $(document).ready(function() {
-    var ctx = $("#chart").get(0).getContext("2d");
-    var burpiChart = new Chart(ctx);
-
     setInterval(function() {
-        update(burpiChart);
+        update();
     }, 1000);
-    update(burpiChart);
-
+    update();
 });
