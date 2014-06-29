e334193f664eae7144374d06fd94394c6aa9a70f
Robert Wallhead <rwallhead@gmail.com>
2014-06-29 15:36:41 +1000

    Fixed up display of date on X axis

--- www/static/burpi.js
+++ www/static/burpi.js
@@ -1,16 +1,31 @@
+function date(epoch) {
+    var date = new Date(0);
+    date.setUTCSeconds(epoch);
+    return date.getTime() - (new Date().getTimezoneOffset() * 60 * 1000);
+}
+
 function update() {
     $.get('/data', function(data) {
         var datasets = [];
         for(sensor in data) {
+            for(i in data[sensor]) {
+                data[sensor][i][0] = date(data[sensor][i][0]);
+            }
             datasets.push(data[sensor]);
         }
-        $.plot("#chart-placeholder", datasets);
+        $.plot("#chart-placeholder", datasets, {
+            xaxis: {
+                mode: "time",
+                timeformat: "%h:%M %d/%m",
+                minTickSize: [10, "minute"]
+            }
+	});
     });
 }
 
 $(document).ready(function() {
     setInterval(function() {
         update();
-    }, 10000);
+    }, 60000);
     update();
 });
